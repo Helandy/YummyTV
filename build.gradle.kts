e9a806417382ba530f11b127919d348167594182
Sandy <3n3my3@gmail.com>
2026-06-13 13:31:25 +0300

    [BUGFIX] Schedule android 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -10,4 +13,27 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+}
+
+val libsCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+val desugarJdkLibs = libsCatalog.findLibrary("desugar-jdk-libs").get()
+
+subprojects {
+    plugins.withId("com.android.application") {
+        extensions.configure<ApplicationExtension>("android") {
+            compileOptions {
+                isCoreLibraryDesugaringEnabled = true
+            }
+        }
+        dependencies.add("coreLibraryDesugaring", desugarJdkLibs)
+    }
+
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension>("android") {
+            compileOptions {
+                isCoreLibraryDesugaringEnabled = true
+            }
+        }
+        dependencies.add("coreLibraryDesugaring", desugarJdkLibs)
+    }
 }
